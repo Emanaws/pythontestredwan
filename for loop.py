3b462d0598e864c2d85d4c1e885c1939342e5589
Emanaws <redwanomer44@gmail.com>
2020-06-14 13:32:22 -0500

    add all totorial

---
+++ for loop.py
@@ -0,0 +1,10 @@
+#student=["redwan","Nejat","Eman","Esan", "Naaol"]
+#for s in student:
+    #print(s)
+
+#name="Redwan Omer"
+#for x in name:
+ #   print(x)
+
+for R in range(50,300,50):
+    print(R)
--- loop.py
+++
@@ -1,7 +0,0 @@
-i=10
-while i<100:
-    print(i)
-    i+=1
-else:
-    print("It is 100")
-  
